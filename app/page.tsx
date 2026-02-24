306f97ffa3c06f51e6d8b6f018f653f6e7d63497
EslamKamel89 <elsamkamelforex@gmail.com>
2026-02-24 23:04:37 +0200

    feat(ui): integrate shadcn button component with utility helpers and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <div>Hello world</div>
+      <Button variant={"secondary"}>Click</Button>
+    </>
+  );
 }
